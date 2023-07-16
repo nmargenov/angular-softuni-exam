c83eefbc91cb53f6497fc23f8c43d9eee4c50a13
nmargenov <nikolay_margenov@yahoo.com>
2023-07-16 14:08:05 +0300

    setup auth middlewares

--- REST API/src/config/expressConfig.js
+++ REST API/src/config/expressConfig.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const { auth } = require('../middlewares/authMiddlewares');
+
 
 exports.expressConfig = (app) =>{
     app.use(express.urlencoded({extended:false}));
     app.use(express.json());
     app.use(cors());
+    app.use(auth);
 }
