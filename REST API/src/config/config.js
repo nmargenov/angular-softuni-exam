0ae97ebaa1807904634676dd2a84dea82bd17c18
nmargenov <nikolay_margenov@yahoo.com>
2023-08-01 19:24:13 +0300

    Added database name to config REST API file. Now to change the database only need to change DATABASE_NAME in config.js

--- REST API/src/config/config.js
+++ REST API/src/config/config.js
@@ -1,4 +1,7 @@
+const DATABASE_NAME = 'angular-softuni-exam';
+
 exports.PORT = 5000;
-exports.URI = 'mongodb://localhost:27017/angular-softuni-exam';
+exports.URI = `mongodb://localhost:27017/${DATABASE_NAME}`;
 exports.SECRET = 'l6zGvJQ9fp7Y8ZiKs4RtD2Ex5A3WhC1nOv0BjUmXwNqTySpFyHdMgVcRoKuIeLxP';
-exports.AUTH_COOKIE = 'x-authorization'
+exports.AUTH_COOKIE = 'x-authorization';
+
